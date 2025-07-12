98433ee6d7298158b7753165114ad1d84a2e1b4f
HardyEra <kashyap.hardik789@gmail.com>
2025-07-12 16:05:26 +0530

    cart option added

--- client/src/user/Navbar.jsx
+++ client/src/user/Navbar.jsx
@@ -1,6 +1,6 @@
 import '../index.css';
 import parseJwt from '../parseJWT';
-
+import {Link} from 'react-router-dom';
 function Navbar() {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     const token = localStorage.getItem('token');
@@ -21,13 +21,14 @@ function Navbar() {
             </div>
 
             <div className="flex items-center space-x-6">
+            <Link to='/userCart'>
                 <button className="relative">
                     🛒
                     <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs px-2 py-0.5 rounded-full">
                         2
                     </span>
                 </button>
-
+            </Link>
                 <div className="text-lg font-medium">
                     Hello, {decoded?.name || 'Guest'}!
                 </div>
